31f9895d7c28ffbd10370a73b7f9a394049b2702
pblaizzz <915892243@qq.com>
2024-08-12 18:49:12 +0800

    嵌套结构体

--- day2/test2.go
+++ day2/test2.go
@@ -13,6 +13,15 @@ func main() {
 	m = 1221
 	m.Dream1()
 	qtStruct() //嵌套结构
+	d1 := &Dog{
+		Feet: 4,
+		Animal: &Animal{ //注意嵌套的是结构体指针
+			name: "乐乐",
+		},
+	}
+	fmt.Println("feet====", (*d1).Feet)
+	d1.wang() //乐乐会汪汪汪~
+	d1.move() //乐乐会动！
 }
 
 type person struct {
@@ -68,4 +77,24 @@ func qtStruct() {
 		},
 	}
 	fmt.Printf("嵌套结构体===%#v", stu)
+}
+
+// 结构体的“继承”
+// Animal 动物
+type Animal struct {
+	name string
+}
+
+func (a *Animal) move() {
+	fmt.Printf("%s会动！\n", a.name)
+}
+
+// Dog 狗
+type Dog struct {
+	Feet    int8
+	*Animal //通过嵌套匿名结构体实现继承
+}
+
+func (d *Dog) wang() {
+	fmt.Printf("%s会汪汪汪~\n", d.name)
 }
